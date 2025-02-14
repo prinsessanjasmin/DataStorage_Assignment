11daad8d44338d5204c8a35c1de3f8fe30f39518
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-02-14 16:41:22 +0100

    Commit 2: Started work on interfaces and repositories.

--- Data/Interfaces/IBaseRepository.cs
+++ Data/Interfaces/IBaseRepository.cs
@@ -1,7 +1,14 @@
-﻿
+﻿using System.Linq.Expressions;
 
-namespace Data.Interfaces;
-
-public interface IBaseRepository
+namespace Data.Interfaces
 {
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<bool> CreateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAsync();
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
+        Task<bool> UpdateAsync(Expression<Func<TEntity, bool>> expression);
+        Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression);
+        Task<bool> AlreadyExistsAsync(Expression<Func<TEntity, bool>> expression); 
+    }
 }
---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,92 @@
+﻿using Data.Contexts;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+
+public abstract class BaseRepository<TEntity>(DataContext context) : IBaseRepository<TEntity> where TEntity : class, IBaseRepository<TEntity>
+{
+    private readonly DataContext _context = context;
+    private readonly DbSet<TEntity> _dbSet = context.Set<TEntity>();
+
+    public virtual async Task<bool> CreateAsync(TEntity entity)
+    {
+        if (entity == null)
+            return false;
+
+        try
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error creating {nameof(TEntity)} :: {ex.Message}");
+            return false;
+        }
+    }
+
+    public Task<IEnumerable<TEntity>> GetAsync()
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            
+        }
+    }
+
+    public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    public Task<bool> UpdateAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    public Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    public Task<bool> AlreadyExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+}
